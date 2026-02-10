693bc8bdefba1a3dff8732ce80a768855b7f760e
Irfan <muhammadirfank2007@gmail.com>
2026-02-10 11:19:02 +0530

    Sidebar Added

--- src/assets/components/homepage-components/Navbar.jsx
+++ src/assets/components/homepage-components/Navbar.jsx
@@ -1,113 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import navbarlogo from "../../image/common-images/logo.webp";
 import "../../css/hompage-css/navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  // State to manage Shop dropdown visibility
+  const [isShopHovered, setIsShopHovered] = useState(false);
+
   return (
-    <nav className="navbar">
+    <nav className="navbar-wrapper">
       <div className="container-fluid" id="navbar-id">
         <div className="container-fluid d-flex justify-content-between py-3 align-items-center navbar-container">
+          {/* Logo Section */}
           <div>
-            <a href="">
+            <Link to="/">
               <img src={navbarlogo} alt="logo" />
-            </a>
+            </Link>
           </div>
 
+          {/* Navigation Links */}
           <div
             className="d-flex justify-content-evenly gap-4 align-items-center"
             id="navbar-links"
           >
-            <div className="d-flex ">
+            <div className="nav-item">
               <h6>
-                <Link
-                  to="/"
-                  style={{ textDecoration: "none", color: "black" }}
-                  className="nav-link-custom"
-                >
+                <Link to="/" className="nav-link-custom">
                   Home
                 </Link>
               </h6>
             </div>
-            <div className="d-flex">
-              <h6>
+
+            {/* SHOP DROPDOWN START */}
+            <div
+              className="nav-item-dropdown"
+              onMouseEnter={() => setIsShopHovered(true)}
+              onMouseLeave={() => setIsShopHovered(false)}
+            >
+              <h6 className="d-flex align-items-center gap-1">
                 <Link
-                  to="/shop"
-                  style={{ textDecoration: "none", color: "black" }}
-                  className="nav-link-custom"
+                  className={`nav-link-custom ${isShopHovered ? "active-green" : ""}`}
                 >
                   Shop
                 </Link>
-              </h6>
-            </div>
-            <div className="d-flex">
-              <h6>
-                <Link
-                  to="/page"
-                  style={{ textDecoration: "none", color: "black" }}
-                  className="nav-link-custom"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  fill={isShopHovered ? "#699c47" : "currentColor"}
+                  viewBox="0 0 16 16"
                 >
+                  <path
+                    fillRule="evenodd"
+                    d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                  />
+                </svg>
+              </h6>
+
+              {/* Dropdown Menu */}
+              <ul className={`dropdown-popup ${isShopHovered ? "show" : ""}`}>
+                <li>
+                  <Link to="/shop">Shop</Link>
+                </li>
+                <li>
+                  <Link to="/wishlist">Wishlist</Link>
+                </li>
+                <li>
+                  <Link to="/cart">Cart</Link>
+                </li>
+                <li>
+                  
+                </li>
+              </ul>
+            </div>
+            {/* SHOP DROPDOWN END */}
+
+            <div className="nav-item">
+              <h6>
+                <Link to="/page" className="nav-link-custom">
                   Pages
                 </Link>
               </h6>
             </div>
-            <div>
+            <div className="nav-item">
               <h6>
-                <Link
-                  to="/blog"
-                  style={{ textDecoration: "none", color: "black" }}
-                  className="nav-link-custom"
-                >
+                <Link to="/blog" className="nav-link-custom">
                   Blog
                 </Link>
               </h6>
             </div>
-            <div>
+            <div className="nav-item">
               <h6>
-                <Link
-                  to="/contact"
-                  style={{ textDecoration: "none", color: "black" }}
-                  className="nav-link-custom"
-                >
+                <Link to="/contact" className="nav-link-custom">
                   Contact
                 </Link>
               </h6>
             </div>
-            <div>
+            <div className="nav-item">
               <h6>
-                <Link
-                  to="/track"
-                  style={{ textDecoration: "none", color: "black" }}
-                  className="nav-link-custom"
-                >
+                <Link to="/track" className="nav-link-custom">
                   Track Order
                 </Link>
               </h6>
             </div>
           </div>
 
+          {/* Search Bar */}
           <div id="search-bar">
-            <div className="d-flex">
-              <div>
-                <input type="text" className="searchbar-input" />
-              </div>
-              <div>
-                <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-search"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                  </svg>
-                </button>
-              </div>
+            <div className="d-flex align-items-center">
+              <input
+                type="text"
+                className="searchbar-input"
+                placeholder="Search..."
+              />
+              <button type="button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+              </button>
             </div>
           </div>
 
+          {/* Icons Section */}
           <div className="d-flex justify-content-evenly gap-4">
             <div className="navbar-icon1">
               <svg
@@ -115,28 +135,24 @@ function Navbar() {
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-bag"
                 viewBox="0 0 16 16"
               >
                 <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
               </svg>
               <span className="navbar-badge">0</span>
             </div>
-
             <div className="navbar-icon1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-heart"
                 viewBox="0 0 16 16"
               >
                 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
               </svg>
               <span className="navbar-badge">0</span>
             </div>
-
             <div className="navbar-icon1" id="navbar-icon-login">
               <Link to="/login" style={{ color: "black" }}>
                 <svg
@@ -144,29 +160,25 @@ function Navbar() {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  class="bi bi-person"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                 </svg>
               </Link>
             </div>
-            <div className="navbar-hamburg">
-              <Link>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-list"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                  />
-                </svg>
-              </Link>
+            <div className="navbar-hamburg d-md-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+                />
+              </svg>
             </div>
           </div>
         </div>
--- src/assets/components/homepage-components/Organictab.jsx
+++ src/assets/components/homepage-components/Organictab.jsx
@@ -7,6 +7,28 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React from "react";
 
+const ProductActionButtons = () => (
+  <>
+    <button className="add-to-cart-btn" id="card-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-plus" viewBox="0 0 16 16">
+        <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z" />
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+      </svg>
+    </button>
+    <button className="add-to-watch-btn" id="card-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+      </svg>
+    </button>
+    <button className="add-to-like-btn" id="card-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
+        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+      </svg>
+    </button>
+  </>
+);
+
 function Organictab() {
   const settings = {
     dots: false,
@@ -124,50 +146,53 @@ function Organictab() {
             <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 ">
               <div className="row  cerosal-box-section">
                 <div className="col-xl-12 col-lg-12 col-md-6 col-sm-6 col-6">
-              <div className="organic-cerosal">
-                <Slider ref={sliderRef} {...settings}>
-                  {productDatas.slice(7, 10).map((product) => (
-                    <div key={product.id}>
-                      <div
-                        className="organic-slide"
-                        style={{
-                          backgroundImage: `url(${product.img})`,
-                        }}
-                      >
-                        <div className="organic-prod">
-                          <h3>${product.price}</h3>
+                  <div className="organic-cerosal">
+                    <Slider ref={sliderRef} {...settings}>
+                      {productDatas.slice(7, 10).map((product) => (
+                        <div key={product.id}>
+                          <div
+                            className="organic-slide"
+                            style={{
+                              backgroundImage: `url(${product.img})`,
+                            }}
+                          >
+                            <div className="organic-prod">
+                              <h3>${product.price}</h3>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                  ))}
-                </Slider>
-              </div>
-              </div>
-              <div className="col-xl-12 col-lg-12 col-md-6 col-sm-6 col-6">
-              <div className="organic-box">
-                <div>
-                  <p>Freshly</p>
-                  <h3>Maitta Potato</h3>
-                  <p>
-                    From <h3>$9.00</h3>
-                  </p>
-                </div>
-                <br />
-                <div className="d-flex align-items-end ">
-                  <div className="organic-crl">
-                    <h5 style={{ color: "white", fontWeight: "bold" }}>Hot</h5>
-                    <h5 style={{ color: "white" }}>item</h5>
+                      ))}
+                    </Slider>
                   </div>
                 </div>
-              </div>
-              </div>
+                <div className="col-xl-12 col-lg-12 col-md-6 col-sm-6 col-6">
+                  <div className="organic-box">
+                    <div>
+                      <p>Freshly</p>
+                      <h3>Maitta Potato</h3>
+                      <span>From</span> <h3>$9.00</h3>
+                    </div>
+                    <br />
+                    <div className="d-flex align-items-end ">
+                      <div className="organic-crl">
+                        <h5 style={{ color: "white", fontWeight: "bold" }}>
+                          Hot
+                        </h5>
+                        <h5 style={{ color: "white" }}>item</h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
               <div className="product-list">
                 <div className="row">
                   {productDatas.slice(0, 4).map((product) => (
-                    <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12" key={product.id}>
+                    <div
+                      className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12"
+                      key={product.id}
+                    >
                       <div className="product-card">
                         <div
                           className="product-card-top"
@@ -181,50 +206,13 @@ function Organictab() {
                             className=" hover-element gap-3 pb-4"
                             style={{ height: "100%" }}
                           >
-                            <button className="add-to-cart-btn" id="card-btn">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                className="bi bi-cart-plus"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z" />
-                                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
-                              </svg>
-                            </button>
-                            <button className="add-to-watch-btn" id="card-btn">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                className="bi bi-eye"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
-                              </svg>
-                            </button>
-                            <button className="add-to-like-btn" id="card-btn">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                className="bi bi-heart"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                              </svg>
-                            </button>
+                            <ProductActionButtons />
                           </div>
                         </div>
 
                         <div className="product-card-bottom text-center">
                           <h4 className="prod-name">
-                            <a href="">{product.name}</a>
+                            <Link to={`/product/${product.id}`}>{product.name}</Link>
                           </h4>
                           <h4 className="prod-price">${product.price}</h4>
                         </div>
@@ -235,7 +223,10 @@ function Organictab() {
                 <br />
                 <div className="row">
                   {productDatas.slice(5, 9).map((product) => (
-                    <div className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12" key={product.id}>
+                    <div
+                      className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12"
+                      key={product.id}
+                    >
                       <div className="product-card">
                         <div
                           className="product-card-top"
